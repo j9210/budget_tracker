5173592d541101c953149f55349bd514d38a6c04
j9210 <71297542+j9210@users.noreply.github.com>
2021-07-03 19:24:33 -0500

    fix files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,10 @@ const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-	"/index.html",
-	"/js/idb.js",
-	"/js/index.js",
-	"/css/styles.css",
+	"./index.html",
+	"./js/idb.js",
+	"./js/index.js",
+	"./css/styles.css",
 ];
 
 self.addEventListener('install', function (e) {
